8d238b57819f4de1fadf3058b8671505e29adc5c
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2021-08-01 17:16:53 -0500

    Planetas y Cámara
    Planetas y Cámara

--- GraficaProyecto/Assets/Done/Done_Scripts/Done_PlayerController.cs
+++ GraficaProyecto/Assets/Done/Done_Scripts/Done_PlayerController.cs
@@ -37,12 +37,12 @@ public class Done_PlayerController : MonoBehaviour
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
 		
-		GetComponent<Rigidbody>().position = new Vector3
+		/*GetComponent<Rigidbody>().position = new Vector3
 		(
 			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
 			0.0f, 
 			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-		);
+		);*/
 		
 		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
