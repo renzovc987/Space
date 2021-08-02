9cb6623e1247c4249e5be36d4bbe2d2b48926baf
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2021-08-02 12:53:32 -0500

    Menu
    Menu

---
+++ GraficaProyecto/Assets/Done/Done_Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+    // Update is called once per frame
+
+}
